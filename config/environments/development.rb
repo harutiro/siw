33c3b8fdff9f360a9097771a2656c73b64c8d49f
harutiro <harutiro2027@outlook.jp>
2021-09-02 13:50:52 +0000

    fix;動かない状態から動く状態に変更 ホワイトリストに追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  config.hosts << "3000-crimson-owl-0ihh6ii5.ws-us16.gitpod.io"
 end
