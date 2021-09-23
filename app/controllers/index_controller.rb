86659e026ae7cf28b78f24c90a30a2beaa277462
harutiro <harutiro2027@outlook.jp>
2021-09-23 02:15:44 +0000

    fix:　カテゴリー変更可能

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,8 +2,8 @@ class IndexController < ApplicationController
     protect_from_forgery
 
     def top
-        @contributions = Item.all
-        @categories = Category.all
+            @contributions = Item.all
+            @categories = Category.all
 
         @user = User.all
     end
@@ -52,5 +52,67 @@ class IndexController < ApplicationController
 
         redirect_to '/'
     end
+
+    def category
+        @categories = Category.all
+        @category = Category.find(params[:id])
+        @category_name = @category.name
+        @contributions = @category.items
+
+        print "======================================\n"
+        print @category.name
+        print "\n"
+        print @category.items
+        print "\n"
+    end
+
+    def good
+        id = params[:id]
+        content = Item.find_by(id: id.to_i)
+        like = content.like
+        content.update({
+            like: like + 1
+        })
+        
+        redirect_to '/'
+    
+    end
+
+    # post '/delete/:id' do
+    #     Contribution.find(params[:id]).destroy
+    #     redirect_to '/'
+    # end
+    
+    # get '/edit/:id' do
+    #     @content = Contribution.find (params[:id])
+    #     @categories = Category.all
+    #     erb :edit
+    # end
+    
+    # post '/renew/:id' do
+        
+    #     user_icon_url = ''
+    #     if params[:user_icon]
+    #         img = params[:user_icon]
+    #         tempfile = img[:tempfile]
+    #         upload = Cloudinary::Uploader.upload(tempfile.path)
+    #         user_icon_url = upload['url']
+    #     end
+    
+    #     content = Contribution.find(params[:id])
+    #     content.update({
+    #         main_title: params[:main_title],
+    #         sub_title: params[:sub_title],
+    #         user_name: params[:user_name],
+    #         message: params[:message],
+    #         url: params[:url],
+    #         like: 0,
+    #         category_id: params[:category],
+    #         pass: params[:pass],
+    #         user_icon: user_icon_url,
+    #     })
+        
+    #     redirect_to '/'
+    # end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "" => "index#top"
   post "create" => "index#create"
+  post "good/:id" => "index#good"
+  get "category/:id" => "index#category"
 
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/logout', to: 'sessions#destroy'
